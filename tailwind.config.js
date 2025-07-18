e384ba66ddd8fe16e54db39d4c916b93ea37646c
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-07-18 06:08:23 -0300

    chore: configurar tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./web1/**/*.{html,js}",
+    "./web2/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
